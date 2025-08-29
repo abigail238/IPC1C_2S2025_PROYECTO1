6fb29c134931412806bca02be042f662f2c5980f
abigail238 <3726005300115@ingenieria.usac.edu.gt>
2025-08-29 17:30:00 -0600

    correccino codigo

--- src/proyecto1/Proyecto1.java
+++ src/proyecto1/Proyecto1.java
@@ -1,9 +1,130 @@
 package proyecto1;
 
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class Proyecto1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+		/*
+		 * public String agregarProducto; public String buscarProducto; public String
+		 * eliminarProducto; public int registrarVenta; public double generarReportes;
+		 * public
+		 */
+
+		Scanner tienda = new Scanner(System.in);
+
+		tienda.useDelimiter("/n");
+
+		int opcion = 0;
+
+		while (opcion < 8) {
+
+			System.out.println(" MENU ");
+
+			System.out.println("1. AGREGAR PRODUCTO");
+
+			System.out.println("2. BUSCAR PRODUCTO");
+
+			System.out.println("3. ELIMINAR PRODUCTO");
+
+			System.out.println("4. REGISTRAR VENTA ");
+
+			System.out.println("5. GENERAR REPORTES");
+
+			System.out.println("6. VER DATOS DEL ESTUDIANTE ");
+
+			System.out.println("7. BITACORA");
+
+			System.out.println("8. SALIR");
+
+			opcion = tienda.nextInt();
+
+			try {
+				System.out.println("Digite una opcion");
+				opcion = tienda.nextInt();
+				String producto;
+				int stock, stockActual;
+
+				switch (opcion) {
+
+				case 1:
+					agregarProducto();
+					break;
+
+				case 2:
+					buscarProducto();
+					break;
+
+				case 3:
+					eliminarProducto();
+					break;
+
+				case 4:
+					registrarProducto();
+					break;
+				case 5:
+					generarReportes();
+					break;
+
+				case 6:
+					verDatosDelEstudiante();
+					break;
+
+				case 7:
+					bitacora();
+					break;
+
+				case 8:
+					salir();
+					break;
+
+				default:
+					break;
+
+				}
+			} catch (Exception e) {
+				System.out.println("jhi");
+			}
+
+		}
+	}
+
+	public static void agregarProducto() { // aqui estamo definiendo nuestos variable
+
+	}
+
+	public static void buscarProducto() {
+		System.out.println("Ingresa el producto");
+	}
+
+	public static void eliminarProducto() {
+		System.out.println("Elimnar producto");
+
+	}
+
+	public static void generarProducto() {
+
+	}
+
+	public static void registrarProducto() {
+
+	}
+
+	public static void generarReportes() {
+
+	}
+
+	public static void verDatosDelEstudiante() {
+
+	}
+
+	public static void bitacora() {
+
+	}
+
+	public static void salir() {
 
 	}
 
