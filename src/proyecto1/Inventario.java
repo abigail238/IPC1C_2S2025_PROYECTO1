f3fe41601c58e16dd34503391e766199d91eb7a4
abigail238 <3726005300115@ingenieria.usac.edu.gt>
2025-09-12 21:12:30 -0600

    buscarProducto

--- src/proyecto1/Inventario.java
+++ src/proyecto1/Inventario.java
@@ -6,67 +6,85 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.Scanner;
 
 public class Inventario {
 
-	private Producto[] datos;
-	private int tamanio;
+	private Producto[] productos;
 
 	public Inventario(int tamanio) {
-		this.datos = new Producto[tamanio];
-		this.tamanio = tamanio;
+		this.productos = new Producto[tamanio];
 	}
 
-	public int getTamanio() {
-		return tamanio;
-	}
-
-	public Producto get(int i) {
-		return datos[i];
+	public Producto getProducto(int i) {
+		return productos[i];
 	}
 
 	public void agregar(Producto p) {
-		for (int i = 0; i < datos.length; i++) {// recorre el array
-			if (datos[i] == null) {
-				datos[i] = p;
+		for (int i = 0; i < productos.length; i++) {// recorre el array
+			if (productos[i] == null) {
+				productos[i] = p;
 				break;
 			}
 		}
 	}
 
+	public boolean buscarProducto(String codigo) {
+
+		boolean encontrado = false;
+		for (int i = 0; i < productos.length; i++) {
+			Producto p = productos[i];
+
+			if (p!=null && p.getCodigo().equalsIgnoreCase(codigo)) {
+				System.out.println("Producto encontrado");
+				System.out.println(p);
+				encontrado = true;
+			}
+		}
+		if (!encontrado) {
+			System.out.println("No se encontro ningun producto con ese codigo");
+		}
+		
+		return encontrado;
+	}
+		
+
+
 	public void guardarEnArchivo(String ruta) throws IOException {
-	    File f = new File(ruta);
+		File f = new File(ruta);
 
-	    // Si no existe el archivo lo crea (FileWriter igual lo hace, pero explícito es más claro)
-	    if (!f.exists()) {
-	        f.createNewFile();
-	        System.out.println("Archivo creado: " + f.getAbsolutePath());
-	    }
-	    
+		// Si no existe el archivo lo crea (FileWriter igual lo hace, pero explícito es
+		// más claro)
+		if (!f.exists()) {
+			f.createNewFile();
+			System.out.println("Archivo creado: " + f.getAbsolutePath());
+		}
+
 		try (BufferedWriter bw = new BufferedWriter(new FileWriter(ruta))) {
-			for (int i = 0; i < datos.length; i++) { // recorre el array
-				bw.write(datos[i].toLinea());
-				bw.newLine();
-
+			for (int i = 0; i < productos.length; i++) { // recorre el array
+				if(productos[i]!=null) {
+					bw.write(productos[i].toLinea());
+					bw.newLine();
+				}
 			}
 		}
 	}
 
 	/** Carga desde texto (reemplaza contenido) */
 	public void cargarDesdeArchivo(String ruta) throws IOException {
-	    File f = new File(ruta);
+		File f = new File(ruta);
 
-	    // Validar si existe
-	    if (!f.exists()) {
-	        System.out.println("El archivo no existe: " + ruta);
-	        return;
-	    }
+		// Validar si existe
+		if (!f.exists()) {
+			System.out.println("El archivo no existe: " + ruta);
+			return;
+		}
 
-	    // Validar si está vacío
-	    if (f.length() == 0) {
-	        System.out.println("El archivo está vacío: " + ruta);
-	        return;
-	    }
+		// Validar si está vacío
+		if (f.length() == 0) {
+			System.out.println("El archivo está vacío: " + ruta);
+			return;
+		}
 		try (BufferedReader br = new BufferedReader(new FileReader(ruta))) {
 			String linea;
 			vaciar();
@@ -79,18 +97,20 @@ public class Inventario {
 	}
 
 	public void vaciar() {
-		for (int i = 0; i < datos.length; i++) {// recorre el array
-			datos[i] = null;
+		for (int i = 0; i < productos.length; i++) {// recorre el array
+			productos[i] = null;
 		}
 	}
 
 	public void listar() {
-		for (int i = 0; i < datos.length; i++) {
-			if(datos[i]!=null) {
-				System.out.println(datos[i]);
+		for (int i = 0; i < productos.length; i++) {
+			if (productos[i] != null) {
+				System.out.println(productos[i]);
 			}
 
 		}
 	}
+	
+
 
 }
--- src/proyecto1/Proyecto1.java
+++ src/proyecto1/Proyecto1.java
@@ -1,20 +1,17 @@
 package proyecto1;
 
-
 import java.io.IOException;
 import java.util.Scanner;
-
 
 public class Proyecto1 {
 
 	private static String archivo = "inventario.txt";
 	private static Inventario inv = new Inventario(100);
-	
 
-	public static void main(String[] args) throws IOException{
+	public static void main(String[] args) throws IOException {
 
 		inv.cargarDesdeArchivo(archivo);
-		Scanner tienda = new Scanner(System.in);
+		Scanner scanner = new Scanner(System.in);
 
 		int opcion = 0;
 
@@ -39,23 +36,23 @@ public class Proyecto1 {
 			System.out.println("8. SALIR");
 			try {
 				System.out.println("Digite un opcion: ");
-				opcion = tienda.nextInt();
-				tienda.nextLine();
+				opcion = scanner.nextInt();
+				scanner.nextLine();
 
 				switch (opcion) {
 
 				case 1:
-					agregarProducto();
+					agregarProducto(scanner);
 
 					break;
 
 				case 2:
-					buscarProducto();
+					buscarProducto(scanner);
 
 					break;
 
 				case 3:
-					eliminarProducto();
+					eliminarProducto(scanner);
 
 					break;
 
@@ -83,71 +80,62 @@ public class Proyecto1 {
 				}
 
 			} catch (Exception e) { // sirve para
-				System.out.println("");
-				System.out.println("Vuelve a intentar, solo se permiten numeros");
-				tienda.next();
+				System.out.println(e.getMessage());
+				System.out.println("Ocurrio un error al ejecutar el programa, vuelve a intentar mas tarde!");
 			}
 
 		}
 	}
 
-	public static void agregarProducto() throws IOException { // aqui estamo definiendo nuestos variable
+	public static void agregarProducto(Scanner scanner) throws IOException { // aqui estamo definiendo nuestos variable
 		System.out.println("Agrega producto");
-		String nombredelproducto = ""; // "" ESTO ES IGUAL A TEXTO VACIO
-		String categoria = "";
-		double precio = 0.0;
-		int cantidadenstock = 0;
-		String codigounicodelproducto = "";
-
-		Scanner scanner = new Scanner(System.in); // cree un nuevo objato llamado scanner
 
 		System.out.println("Nombre del producto");
-		nombredelproducto = scanner.nextLine();
-		System.out.println("Categoria");
-		categoria = scanner.nextLine();
-		System.out.println("Precio");
-		precio = scanner.nextDouble();
-		System.out.println("Cantidad en stock");
-		cantidadenstock = scanner.nextInt();
-		System.out.println("Codigo unico del producto");
-		codigounicodelproducto = scanner.nextLine();
+		String nombredelproducto = scanner.nextLine();
 
-		Producto producto = new Producto();
-		producto.setNombre(nombredelproducto);
-		producto.setCategoria(categoria);
-		producto.setPrecio(precio);
-		producto.setCantidad(cantidadenstock);
-		producto.setCodigo(codigounicodelproducto);
-		
-		// Vamos a validar 
-		if (precio <=0) {
-			System.out.println("El precio debe ser mayor a 0");
-			return;
+		System.out.println("Categoria");
+		String categoria = scanner.nextLine();
+
+		System.out.println("Precio");
+		double precio = scanner.nextDouble();
+		// Vamos a validar2
+		if (precio <= 0) {
+			throw new RuntimeException("El precio debe ser mayor a 0");
 		}
+
+		System.out.println("Cantidad en stock");
+		int cantidadenstock = scanner.nextInt();
 		if (cantidadenstock < 0) {
-			System.out.println("La cantidad no puede ser negativa");
-			return;
+			throw new RuntimeException("La cantidad en stock debe ser mayor a cero.");
 		}
-		if (existeCodigo(codigounicodelproducto)) {
-			System.out.println("El codigo ya existe");
-			return;
+
+		scanner.nextLine();
+		
+		System.out.println("Codigo unico del producto");
+		String codigounicodelproducto = scanner.nextLine();
+		if (inv.buscarProducto(codigounicodelproducto)) {
+			throw new RuntimeException("El codigo ya existe");
 		}
+
+		Producto producto = new Producto(codigounicodelproducto,nombredelproducto,categoria,precio,cantidadenstock);
 
 		inv.agregar(producto);
 		inv.guardarEnArchivo(archivo);
 	}
 
-	private static boolean existeCodigo(String codigounicodelproducto) {
-		// TODO Auto-generated method stub
-		return false;
-	}
 
-	public static void buscarProducto() {
+
+	public static void buscarProducto(Scanner scanner) {
 		System.out.println("Ingresa el producto");
 
+		System.out.println("Ingrese el codigo del producto");
+		String codigo = scanner.nextLine();
+
+		inv.buscarProducto(codigo);
+
 	}
 
-	public static void eliminarProducto() {
+	public static void eliminarProducto(Scanner scanner) {
 		System.out.println("Elimnar producto");
 
 	}
@@ -165,7 +153,7 @@ public class Proyecto1 {
 	}
 
 	public static void verDatosDelEstudiante() {
-		
+
 		System.out.println("Estudiante");
 		System.out.println("Nombre: Lorena Abigail Arrecis Martinez");
 
